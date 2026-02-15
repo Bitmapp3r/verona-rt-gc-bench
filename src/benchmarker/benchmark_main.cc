5efdf5ba22d2558711d0501af2bc5ddc44965613
yasin <yasinakkhan04@gmail.com>
2026-02-15 23:34:24 +0000

    feat: test files can now be included from benchmarker

--- src/benchmarker/benchmark_main.cc
+++ src/benchmarker/benchmark_main.cc
@@ -1,6 +1,55 @@
-#include <iostream>
+#include "debug/logging.h"
+#include "util/gc_benchmark.h"
+#include "func/arbitrary_nodes/arbitrary_nodes.h"
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+#include <debug/harness.h>
+#include <iostream>
+#include <test/opt.h>
+
+int main(int argc, char** argv)
+{
+  opt::Opt opt(argc, argv);
+
+  // Default values
+  int size = 1010;
+  int regions = 100;
+  bool enable_log = true;
+
+  // Parse command line arguments
+  if (argc >= 3)
+  {
+    size = std::atoi(argv[1]);
+    regions = std::atoi(argv[2]);
+  }
+
+  if (argc >= 4)
+  {
+    std::string log_arg = argv[3];
+    if (log_arg == "log")
+    {
+      enable_log = true;
+    }
+  }
+
+  // Enable logging if requested
+  if (enable_log)
+  {
+    Logging::enable_logging();
+  }
+  size_t runs = 10;
+  size_t warmup_runs = 10;
+
+  SystematicTestHarness harness(argc, argv);
+
+  std::cout << "\nRunning with arena region" << std::endl;
+  GCBenchmark trace_benchmark;
+
+  trace_benchmark.run_benchmark(
+    [&, size, regions]() {
+      harness.run(
+        [=]() { arbitrary_nodes::run_test<RegionType::Trace>(size, regions); });
+    },
+    runs,
+    warmup_runs);
+  trace_benchmark.print_summary("Arbitrary Nodes - Using Trace");
 }
