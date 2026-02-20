37ff14b5600fff2ed10ce2cdd644480c0031b300
Tausif Samad <ts2623@ic.ac.uk>
2026-02-20 01:13:56 +0000

    Fix RC garbage collection bugs
    - Fixed `rc_dealloc_lins_stack_elem` bug by implementing and calling a
      `remove` method on the lins stack to remove objects from the stack
      before they are freed -- runs in O(n) time.
    - Fixed `rc_distant_cycle` bug by additionally facilitating pushes to
      the lins stack in `dealloc_object()` instead of just in direct
      `decref()` calls.

--- src/rt/region/region_rc.h
+++ src/rt/region/region_rc.h
@@ -162,10 +162,6 @@ namespace verona::rt
     static void incref(Object* o)
     {
       o->incref_rc_region();
-
-      // TODO: Currently we don't remove items from the Lins stack because
-      // there is no way to find the object in the stack without an O(n) pass.
-      // This is still technically correct, but inefficient.
     }
 
     /// Decrements the reference count of `o`. The object `in` is the entry
@@ -615,6 +611,17 @@ namespace verona::rt
               p->finalise(nullptr, sub_regions);
               gc.push(p);
             }
+            else
+            {
+              // RC is still > 0, so this object may be part of a cycle.
+              // Push it onto the Lins stack as a candidate for cycle
+              // collection, matching the behaviour of decref().
+              if (p->get_rc_colour() != RcColour::BLACK)
+              {
+                p->set_rc_colour(RcColour::BLACK);
+                reg->lins_stack.push(p);
+              }
+            }
             break;
           case Object::SCC_PTR:
             p->immutable();
@@ -640,6 +647,9 @@ namespace verona::rt
         Object* o = gc.pop();
         reg->region_size -= 1;
         reg->current_memory_used -= o->size();
+        // Remove from the Lins stack before freeing to prevent gc_cycles
+        // from later processing a stale (dangling) pointer.
+        reg->lins_stack.remove(o);
         o->destructor();
         o->dealloc();
       }
