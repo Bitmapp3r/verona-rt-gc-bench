8554270f2295a8204e2b6b6c5016e5d756fabec7
yasin <yasinakkhan04@gmail.com>
2026-03-11 15:05:22 +0000

    fix: change father to work on windows and linux

--- test/benchmarks/father/father.cc
+++ test/benchmarks/father/father.cc
@@ -2,31 +2,19 @@
 // SPDX-License-Identifier: MIT
 #include "father.h"
 
-#include <debug/harness.h>
-#include <test/opt.h>
-#include <iostream>
-#include <util/gc_benchmark.h>
+#include "../../../src/benchmarker/export_macro.h"
 #include "../benchmarker/benchmark_main_helper.h"
 #include "region/region_base.h"
-#include "../../../src/benchmarker/export_macro.h"
 
+#include <cstddef>
+#include <debug/harness.h>
+#include <iostream>
+#include <test/opt.h>
+#include <util/gc_benchmark.h>
+#include <string>
 using namespace verona::rt::api;
 
-#if defined(_WIN32) || defined(_WIN64)
-extern "C" BENCHMARK_EXPORT void set_gc_callback(void (*callback)(uint64_t, verona::rt::RegionType, size_t, size_t))
-{
-  static std::function<void(uint64_t, verona::rt::RegionType, size_t, size_t)> func;
-  if (callback)
-  {
-    func = callback;
-    verona::rt::set_gc_callback(&func);
-  }
-  else
-  {
-    verona::rt::set_gc_callback(nullptr);
-  }
-}
-#endif
+MAKE_REGION_WRAPPER(test, father::run_test);
 
 extern "C" BENCHMARK_EXPORT int run_benchmark(int argc, char** argv)
 {
@@ -39,63 +27,41 @@ extern "C" BENCHMARK_EXPORT int run_benchmark(int argc, char** argv)
 
   // Parse benchmark parameters
   size_t iterations = opt.is<size_t>("--iterations", 5);
+  size_t num_regions = opt.is<size_t>("--num-regions", 1);
 
-  // Set up workloads: 1 GOL + 1 grid_walkers
   std::vector<std::pair<std::string, std::vector<std::string>>> workloads;
-
-//   // Grid walkers workload
-  // workloads.push_back({
-  //   "test\\benchmarks\\grid_walkers\\con-library\\Debug\\benchmarks-con-grid_walkers_lib.dll",
-  //   {}
-  // });
-
+  
+  std::string pointer_churn_with_concurrencyPath = "test/benchmarks/pointer_churn_with_concurrency/con-library/";
+  #ifdef PLATFORM_WINDOWS
+  pointer_churn_with_concurrencyPath += "Debug/";
+  #endif
+  std::string pointerChurnLib = buildLibraryPath(
+      pointer_churn_with_concurrencyPath,
+      "benchmarks-con-pointer_churn_lib_with_concurrency"
+  );
+  
   workloads.push_back({
-    "test\\benchmarks\\pointer_churn\\con-library\\Debug\\benchmarks-con-pointer_churn_lib.dll",
-    {"test\\benchmarks\\pointer_churn\\con-library\\Debug\\benchmarks-con-pointer_churn_lib.dll", "--arena", "--m", "1000"}
+      pointerChurnLib,
+      {pointerChurnLib, "-n", "10000", "--regions", "30", "--iterations", "100", "--m", "10000"}
   });
-  //Best Arena, Worst RC
-
-  // GOL workload
+  
+  std::string golPath = "test/benchmarks/gol/con-library/";
+  #ifdef PLATFORM_WINDOWS
+  golPath += "Debug/";
+  #endif
+  std::string golLib = buildLibraryPath(
+      golPath,
+      "benchmarks-con-gol_lib"
+  );
+  
   workloads.push_back({
-    "test\\benchmarks\\gol\\con-library\\Debug\\benchmarks-con-gol_lib.dll",
-    {".\test\benchmarks\gol\con-library\Debug\benchmarks-con-gol_lib.dll", "--arena", "--generations", "10"}
+      golLib,
+      {golLib, "--arena", "--generations", "10000"}
   });
-  // Best RC/Arena, Worst Trace
-
-  workloads.push_back({
-    "test\\benchmarks\\reproduction\\con-library\\Debug\\benchmarks-con-reproduction_lib.dll",
-    {"test\\benchmarks\\reproduction\\con-library\\Debug\\benchmarks-con-reproduction_lib.dll","--arena", "--generations", "300", "--killPercent", "50", "--popSize", "100"}
-    //Best Trace, Worst Arena
-  });
-
-  size_t num_regions = 1 ;
-
-  SystematicTestHarness harness(argc, argv);
-
-  if (rt == RegionType::Trace)
-  {
-    harness.run([&]() {
-      father::run_test<RegionType::Trace>(workloads, num_regions, iterations);
-    });
-  }
-  else if (rt == RegionType::Rc)
-  {
-    harness.run([&]() {
-      father::run_test<RegionType::Rc>(workloads, num_regions, iterations);
-    });
-  }
-  else if (rt == RegionType::Arena)
-  {
-    harness.run([&]() {
-      father::run_test<RegionType::Arena>(workloads, num_regions, iterations);
-    });
-  }
+  
+  DISPATCH_REGION(rt, test, workloads, num_regions, iterations);
 
   return 0;
 }
 
-int main(int argc, char** argv)
-{
-  run_benchmark(argc, argv);
-  return 0;
-}
+RUN_BENCHMARK_MAIN()
